319fe0cc89bdf1c62266fbb5e83673ef41cb5ef3
Natalie <semmlenr@evms.edu>
2020-10-09 15:41:04 -0400

    ugh

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
+/*const modal = document.getElementById('modal')
+const card = document.getElementById('card')
+const closeModal = document.getElementById('closeModal')
+
+card.addEventListener('click', showModal)
+closeModal.addEventListener('click', hideModal)
+
+function showModal(source) {
+  modal.style.display = "block";
+}
+
+function hideModal() {
+  modal.style.display = 'none'
+}
+NEW*/
 // Get each modal and close button
 const triggers = document.getElementsByClassName('trigger');
 const triggerArray = Array.from(triggers).entries();
 const modals = document.getElementsByClassName('card__modal');
 const closeButtons = document.getElementsByClassName('card__modal__close');
-const modalVideo = document.getElementById('modalVideo');
-
-function disableScroll() {
-  html.style.overflow = "hidden";
-}
-function enableScroll() {
-  html.style.overflow = null;
-}
 
 // Then use `for...of`-loop with the index of each item in `triggerArray` for listening to a click event which toggles each modal to open and close
 for (let [index, trigger] of triggerArray) {
-  let triggerIndex = index;
-  function toggleModal() {
-    // Optionally toggle a class for CSS animations
-    modals[triggerIndex].classList.toggle('show-modal');
-    if (modals[triggerIndex].classList.contains('show-modal')) {
-      disableScroll();
-      let lazyVids = document.getElementsByTagName('iframe');
-          if (lazyVids.getAttribute('data-src')) {
-              lazyVids.setAttribute('src', lazyVids.getAttribute('data-src'));
-          }
-    } else {
-      card__modal.style.display = "none";
-      /* modalVideo.src = iframe.src + '?autoplay=0'; */
-      enableScroll();
-      lazyVids.setAttribute('src',"");
-    }
-  }
+let triggerIndex = index;
+function toggleModal() {
+  // Optionally toggle a class for CSS animations
+modals[triggerIndex].classList.toggle('show-modal');
 }
 trigger.addEventListener("click", toggleModal);
 closeButtons[triggerIndex].addEventListener("click", toggleModal);
-    /* closeModal[triggerIndex].addEventListener("click", function () {
-  card__modal.style.display = "none";
-  modalVideo.src = iframe.src + '?autoplay=0';
-  enableScroll();
-});;*/
+}
 
+// Get each modal and close button
+/*const triggersTitle = document.getElementsByClassName('trigger__title');
+const triggerArrayTitle = Array.from(triggersTitle).entries();
+const modalsTitle = document.getElementsByClassName('card__modal');
+const closeButtonsTitle = document.getElementsByClassName('card__modal__close');
 
+// Then use `for...of`-loop with the index of each item in `triggerArray` for listening to a click event which toggles each modal to open and close
+for (let [index, trigger__title] of triggerArrayTitle) {
+let triggerIndexTitle = index;
+function toggleModalTitle() {
+  // Optionally toggle a class for CSS animations
+modalsTitle[triggerIndexTitle].classList.toggle('show-modal');
+}
+trigger__title.addEventListener("click", toggleModalTitle);
+closeButtonsTitle[triggerIndexTitle].addEventListener("click", toggleModalTitle);
+}*/
---
+++ index1.js
@@ -0,0 +1,43 @@
+// Get each modal and close button
+const triggers = document.getElementsByClassName('trigger');
+const triggerArray = Array.from(triggers).entries();
+const modals = document.getElementsByClassName('card__modal');
+const closeButtons = document.getElementsByClassName('card__modal__close');
+const modalVideo = document.getElementById('modalVideo');
+
+function disableScroll() {
+  html.style.overflow = "hidden";
+}
+function enableScroll() {
+  html.style.overflow = null;
+}
+
+// Then use `for...of`-loop with the index of each item in `triggerArray` for listening to a click event which toggles each modal to open and close
+for (let [index, trigger] of triggerArray) {
+  let triggerIndex = index;
+  function toggleModal() {
+    // Optionally toggle a class for CSS animations
+    modals[triggerIndex].classList.toggle('show-modal');
+    if (modals[triggerIndex].classList.contains('show-modal')) {
+      disableScroll();
+    } else {
+      card__modal.style.display = "none";
+      modalVideo.src = iframe.src + '?autoplay=0'; 
+      enableScroll();
+    }
+  }
+}
+trigger.addEventListener("click", toggleModal);
+closeButtons[triggerIndex].addEventListener("click", toggleModal);
+/* closeModal[triggerIndex].addEventListener("click", function () {
+  card__modal.style.display = "none";
+  modalVideo.src = iframe.src + '?autoplay=0';
+  enableScroll();
+});;*/
+/* let lazyVids = document.getElementsByTagName('iframe');
+    if (lazyVids.getAttribute('data-src')) {
+        lazyVids.setAttribute('src', lazyVids.getAttribute('data-src'));
+    }
+    lazyVids.setAttribute('src',""); */
+
+
